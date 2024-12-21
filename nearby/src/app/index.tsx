34514717a3eef33b8127ea7320f7ede9863a562c
christyansegecs <christyansegecs@hotmail.com>
2024-12-21 17:27:47 -0300

    Aula 2 B

--- nearby/src/app/index.tsx
+++ nearby/src/app/index.tsx
@@ -1,7 +1,11 @@
 
+import { View, Text } from 'react-native'
+import { router } from 'expo-router'
+import { api } from '@/services/api'
+
 import { Steps } from '@/components/steps'
 import { Welcome } from '@/components/welcome'
-import { View, Text } from 'react-native'
+import { Button } from '@/components/button'
 
 export default function Index() {
     
@@ -9,6 +13,9 @@ export default function Index() {
         <View style={{ flex: 1, padding: 40, gap: 40 }}>
             <Welcome />
             <Steps />
+            <Button onPress={() => router.navigate('/home')}>
+                <Button.Title>Começar</Button.Title>
+            </Button>
         </View>
     )
 }
